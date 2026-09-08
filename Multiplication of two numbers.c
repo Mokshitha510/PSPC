24480d728e5b9e146361fa615578f785f5f5705a
Mokshitha510 <mokshitha.morampudi@gmail.com>
2026-09-08 18:38:08 +0530

    Create Multiplication of two numbers.c

---
+++ Multiplication of two numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int x=0;
+    int y=0;
+    int result=0;
+    printf("Enter your first number\n");
+    scanf("%d",&x);
+    printf("Enter your second number\n");
+    scanf("%d", &y);
+    result = x*y;
+    printf("The result of multiplication is: %d", result);
+    return 0;
+}
